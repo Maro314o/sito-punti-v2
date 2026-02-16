9e219b7a038d40ed766cb1d679ad0eca4507d79b
Gentih <michele.gentilini.2008@gmail.com>
2026-02-16 18:20:35 +0100

    added navbar. NEEDS LINKS

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import ButtonsWrapper from "@/components/modules/ButtonsWrapper/page";
 import FormWrapper from "@/components/modules/FormWrapper/page";
+import Navbar from "@/components/modules/Navbar/page";
 import TitleWrapper from "@/components/modules/TitleWrapper/page";
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <TitleWrapper title={"One Piece"} />
       <FormWrapper />
       <ButtonsWrapper />
---
+++ components/modules/Navbar/page.tsx
@@ -0,0 +1,111 @@
+"use client";
+
+import * as React from "react";
+import Link from "next/link";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+
+export default function Navbar() {
+  return (
+    <div className="pt-10 fixed w-full">
+      <NavigationMenu className="border bg-background shadow-xs whitespace-nowrap rounded-md">
+        <div className="flex md:hidden">
+          <MobileContent />
+        </div>
+        <div className="hidden md:flex">
+          <DesktopContent />
+        </div>
+      </NavigationMenu>
+    </div>
+  );
+}
+
+function ListItem({
+  title,
+  href,
+  ...props
+}: React.ComponentPropsWithoutRef<"li"> & { href: string }) {
+  return (
+    <li {...props}>
+      <NavigationMenuLink asChild>
+        <Link href={href}>
+          <div className="flex flex-col gap-1 text-sm">
+            <div className="leading-none font-medium">{title}</div>
+          </div>
+        </Link>
+      </NavigationMenuLink>
+    </li>
+  );
+}
+
+const MobileContent = () => {
+  return (
+    <NavigationMenuList>
+      <NavigationMenuItem>
+        <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+          <Link href="/">Home</Link>
+        </NavigationMenuLink>
+      </NavigationMenuItem>
+      <NavigationMenuItem>
+        <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+          <Link href="/">Profilo</Link>
+        </NavigationMenuLink>
+      </NavigationMenuItem>
+      <NavigationMenuItem>
+        <NavigationMenuTrigger>Informazioni</NavigationMenuTrigger>
+        <NavigationMenuContent>
+          <ul className="w-66">
+            <ListItem href="/" title="Caccia al tesoro"></ListItem>
+            <ListItem href="/" title="Rotte"></ListItem>
+            <ListItem href="/" title="Misteri"></ListItem>
+            <ListItem href="/" title="Regole"></ListItem>
+          </ul>
+        </NavigationMenuContent>
+      </NavigationMenuItem>
+    </NavigationMenuList>
+  );
+};
+
+const DesktopContent = () => {
+  return (
+    <NavigationMenuList>
+      <NavigationMenuItem>
+        <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+          <Link href="/">Home</Link>
+        </NavigationMenuLink>
+      </NavigationMenuItem>
+      <NavigationMenuItem>
+        <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+          <Link href="/">Profilo</Link>
+        </NavigationMenuLink>
+      </NavigationMenuItem>
+      <NavigationMenuItem>
+        <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+          <Link href="/">Caccia al tesoro</Link>
+        </NavigationMenuLink>
+      </NavigationMenuItem>
+      <NavigationMenuItem>
+        <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+          <Link href="/">Rotte</Link>
+        </NavigationMenuLink>
+      </NavigationMenuItem>
+      <NavigationMenuItem>
+        <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+          <Link href="/">Misteri</Link>
+        </NavigationMenuLink>
+      </NavigationMenuItem>
+      <NavigationMenuItem>
+        <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
+          <Link href="/">Regole</Link>
+        </NavigationMenuLink>
+      </NavigationMenuItem>
+    </NavigationMenuList>
+  );
+};
